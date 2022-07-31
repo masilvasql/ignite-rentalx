046aa2637a6753a677f1d2ccacbfd1b94a49d849
Marcelo Abrão <masilvasql@gmail.com>
2022-07-30 22:54:29 -0300

    inciando com docker compose, iniciando criação de arquivos de configuração do banco de dados

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "rentx",
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
